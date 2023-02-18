4dea700d287f9f9cc3d8c47e8626a49083309c6b
medherradi <med.herradi@gmail.com>
2023-02-18 22:42:53 -0100

    commit update serializer

--- API/post/api/views.py
+++ API/post/api/views.py
@@ -2,6 +2,7 @@ from ..models import Post
 from django.shortcuts import get_object_or_404
 from .serializers import PostSerializer
 from rest_framework import status
+from rest_framework.generics import CreateAPIView, ListCreateAPIView, DestroyAPIView, RetrieveUpdateAPIView
 from rest_framework.decorators import action, api_view
 from rest_framework.response import Response
 
@@ -15,26 +16,30 @@ def post_list(request):
         content = {"count": count, "data": serializer.data}
         return Response(content,  status=status.HTTP_200_OK)
     elif request.method == 'POST':
-        print(request)
+        print(request.data)
         serializer = PostSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        s
-        return Response('ok')
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'PATCH'])
+@api_view(['GET', 'PATCH', 'DELETE'])
 def post_detail(request, **kwargs):
     pk = kwargs.get('pk')
     print(pk)
     post = get_object_or_404(Post, id=pk)
     if request.method == 'GET':
         serializer = PostSerializer(post)
-        content = {
-            'msg': f'post with title {post.title}', 'data': serializer.data}
-        return Response(content, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == 'PATCH':
         serializer = PostSerializer(instance=post, data=request.data)
         serializer.is_valid(raise_exception=True)
+        serializer.save()
         content = {
             "msg": f'post with title {post.title} has been updated', 'data': serializer.data}
         return Response(content, status=status.HTTP_201_CREATED)
+    elif request.method == 'DELETE':
+        post.delete()
+        content = {
+            'msg': f'post with title {post.title} has been deleted successfully'}
+        return Response(content, status=status.HTTP_200_OK)
